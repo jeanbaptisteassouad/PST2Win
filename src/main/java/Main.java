072ac37cb8f91c3c7552c87f06ca3803b80facaa
Jean-Baptiste Assouad <jean.baptiste@assouad.com>
2018-02-20 15:30:53 +0100

    Minimal Eclipse Project with Maven and libpst

---
+++ src/main/java/Main.java
@@ -0,0 +1,25 @@
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import com.pff.*;
+
+public class Main {
+	public static void main(String[] args) {
+		String filename = "/Users/jean-baptiste/Desktop/backup.pst";
+		try {
+			PSTFile pstFile = new PSTFile(filename);
+			System.out.println(pstFile.getMessageStore().getDisplayName());
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (PSTException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        System.out.println("Hello World!"); // Display the string.
+    }
+}
